0c544f5688dcf3e6533246c108b5bb863968be93
limchaehwan <chlim20@naver.com>
2024-03-23 00:26:18 +0900

    bronze2480

---
+++ src/baekjoonBronze/Bronze2742.java
@@ -0,0 +1,16 @@
+package baekjoonBronze;
+
+import java.util.Scanner;
+
+public class Bronze2742 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = scanner.nextInt();
+
+        for(int i=num;i>=1;i--)
+        {
+            System.out.println(i);
+        }
+    }
+}
